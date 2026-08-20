46788df74038e0c38aa3a05472d49eb5e872964f
mohammedsaleh077 <mohammed.saleh@tuta.io>
2026-08-20 14:46:19 +0300

    done task 2

--- Day4/script.js
+++ Day4/script.js
@@ -1,3 +1,5 @@
+// Task 1
+
 var images_list = ['1.jpg', '2.webp', '3.png', '4.png', '5.jpg', '6.jpeg', '7.jpg'];
 
 var next_btn = document.getElementById("next")
@@ -12,9 +14,9 @@ var slide_show;
 function changeImage(ptr_move = 0) {
     if (ptr + ptr_move > images_list.length - 1) {
         ptr = 0
-    } else if( ptr+ptr_move < 0){
+    } else if (ptr + ptr_move < 0) {
         ptr = images_list.length - 1
-    }else {
+    } else {
         ptr += ptr_move
     }
     image.src = `./imgs/${images_list[ptr]}`;
@@ -37,7 +39,55 @@ slide_btn.addEventListener("click", function () {
     }
 })
 
-stop_btn.addEventListener("click", function(){
+stop_btn.addEventListener("click", function () {
     clearInterval(slide_show)
     slide_show = null;
 })
+
+// Task 2
+var add_btn = document.getElementById("add")
+var name_input = document.getElementById("name")
+var age_input = document.getElementById("age")
+var crud_table = document.getElementById("crud")
+
+var Name, age, item = 0, edit_mode = false
+add_btn.addEventListener("click", function () {
+    Name = document.getElementById("name").value
+    age = document.getElementById("age").value
+    if (!edit_mode) {
+        crud_table.innerHTML += `
+        <tr id="item${item}">
+            <td id="item${item}_n">${Name}</td>
+            <td id="item${item}_a">${age}</td>
+            <td>
+                <button onclick="edit('item${item}')">Edit</button>
+                <button onclick="Delete('item${item}')">Delete</button>
+            </td>
+        </tr>`
+        item++;
+        name_input.value = ""
+        age_input.value = ""
+    }else{
+        var e_name = document.getElementById(edit_mode + "_n")
+        var e_age = document.getElementById(edit_mode + "_a")
+        e_name.innerText = Name
+        e_age.innerText = age
+        edit_mode = false
+        add_btn.value = "Add"
+        name_input.value = ""
+        age_input.value = ""
+
+    }
+})
+
+function edit(tr_id) {
+    edit_mode = tr_id;
+    add_btn.value = "Edit"
+    
+    name_input.value = document.getElementById(tr_id + "_n").innerText
+    age_input.value = document.getElementById(tr_id + "_a").innerText
+}
+
+function Delete(tr_id){
+    document.getElementById(tr_id).remove()
+}
